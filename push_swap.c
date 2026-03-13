bf96e21482029104330e82fc0de7486be6b0874c
Ilyes Naciri <inaciri@k2r2p10.42mulhouse.fr>
2026-03-13 14:32:28 +0100

    fix: removed printlist in push_swap

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: inaciri <inaciri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 16:28:05 by inaciri           #+#    #+#             */
-/*   Updated: 2026/03/12 17:21:04 by inaciri          ###   ########.fr       */
+/*   Updated: 2026/03/13 14:32:03 by inaciri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,11 +36,7 @@ void	ft_push_swap(t_list **stack_a, t_list **stack_b, t_oper **op, p_list **para
 		ft_lstclear_all(stack_a, stack_b, param, op);
 		return ;
 	}
-	printf("AVANT set_alg - Stack A:\n");
-    printlist(*stack_a);
 	set_alg(param, stack_a, stack_b, op);
-	printf("APRES set_alg - Stack A:\n");
-    printlist(*stack_a);
 	ft_lstclear_all(stack_a, stack_b, param, op);
 }
 
